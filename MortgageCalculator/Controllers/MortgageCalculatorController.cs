c6128845f694968bbd96b6391728f4f14c27bd02
Christopher Perez <christopher.perez19kdc@gmail.com>
2024-03-06 18:47:08 -0500

    after many attempts got it to work,

---
+++ MortgageCalculator/Controllers/MortgageCalculatorController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MortgageCalculator.Models;
+
+namespace MortgageCalculator.Controllers
+{
+    public class MortgageCalculatorController : Controller
+    {
+        // GET: MortgageCalculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: MortgageCalculator/Calculate
+        [HttpPost]
+        public ActionResult Calculate(MortgageCalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                decimal monthlyPayment = model.CalculateMonthlyPayment();
+                ViewBag.MonthlyPayment = monthlyPayment;
+
+                // Redirect to the Calculate view
+                return View("Calculate", model);
+            }
+
+            // If model state is not valid, return to the input form view (Index)
+            return View("Index", model);
+        }
+    }
+
+    }
---
+++ MortgageCalculator/Models/MortgageCalculatorModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MortgageCalculator.Models
+{
+    public class MortgageCalculatorModel
+    {
+
+        public decimal LoanAmount { get; set; }
+        public decimal InterestRate { get; set; } 
+        public int LoanTermYears { get; set; } 
+
+        // calculate monthly payment
+        public decimal CalculateMonthlyPayment()
+        {
+            decimal monthlyInterestRate = InterestRate / 12 / 100;
+            int totalPayments = LoanTermYears * 12; 
+
+            decimal monthlyPayment = (LoanAmount * monthlyInterestRate) /
+                                      (1 - (decimal)Math.Pow(1 + (double)monthlyInterestRate, -totalPayments));
+            return monthlyPayment;
+        }
+    }
+}
