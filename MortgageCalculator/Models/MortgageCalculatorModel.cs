1f9d40326fda749a1bc4b255da05807d4bed48cb
Christopher Perez <christopher.perez19kdc@gmail.com>
2024-03-07 18:03:06 -0500

    Updated Mortgage Calculator and more details are avaiable

--- MortgageCalculator/Models/MortgageCalculatorModel.cs
+++ MortgageCalculator/Models/MortgageCalculatorModel.cs
@@ -5,15 +5,18 @@
         public double LoanAmount { get; set; }
         public double InterestRate { get; set; }
         public int LoanTermMonths { get; set; }
-
+        public double PurchasePrice {  get; set; }
+        public double DownAmount {  get; set; }
         public double MonthlyPayment { get; private set; }
         public double TotalPayment { get; private set; }
 
         public void Calculate()
         {
+            double purchasePrice = PurchasePrice;
+            double downAmount = DownAmount;
             double monthlyInterestRate = InterestRate / 100 / 12;
             double totalMonths = LoanTermMonths;
-            double loanAmount = LoanAmount;
+            double loanAmount = (LoanAmount = (purchasePrice - downAmount));
 
             double monthlyPayment = loanAmount * monthlyInterestRate / (1 - Math.Pow(1 + monthlyInterestRate, -totalMonths));
             MonthlyPayment = Math.Round(monthlyPayment, 2);
