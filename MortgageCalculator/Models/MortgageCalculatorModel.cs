1d56ab124db2ebe60e0b0f73ada9b03b2839a1fe
Christopher Perez <christopher.perez19kdc@gmail.com>
2024-03-08 22:35:48 -0500

    Changed colors, updated results to make a more clear and detailed results

--- MortgageCalculator/Models/MortgageCalculatorModel.cs
+++ MortgageCalculator/Models/MortgageCalculatorModel.cs
@@ -4,20 +4,26 @@
     {
         public double LoanAmount { get; set; }
         public decimal InterestRate { get; set; }
-        public int LoanTermMonths { get; set; }
+        
         public double PurchasePrice {  get; set; }
         public double DownAmount {  get; set; }
         public double MonthlyPayment { get; private set; }
         public double TotalPayment { get; private set; }
+        public double Years {  get; set; }
+        public double InMonths {  get; set; }
 
+        public double DownAmountPercent {  get; set; }
         public void Calculate()
         {
+           
             double purchasePrice = PurchasePrice;
             double downAmount = DownAmount;
             double monthlyInterestRate = (double)(InterestRate / 100 / 12);
-            double totalMonths = LoanTermMonths;
+            double downPercent = (DownAmountPercent =(downAmount / purchasePrice) * 100);
+            
+            double totalMonths = (Years * 12);
             double loanAmount = (LoanAmount = (purchasePrice - downAmount));
-
+            double inMonths = (InMonths = (totalMonths));
             double monthlyPayment = loanAmount * monthlyInterestRate / (1 - Math.Pow(1 + monthlyInterestRate, -totalMonths));
             MonthlyPayment = Math.Round(monthlyPayment, 2);
 
